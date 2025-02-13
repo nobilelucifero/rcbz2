ef50c2e2516954fe288f813d9126142966faaeef
Riccardo Buzzotta <riccardo.buzzotta@gmail.com>
2025-02-13 12:31:40 +0100

    fix styling of image:

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Riccardo Buzzotta";
-export const SITE_DESCRIPTION = "Product designer and entrepreneur";
+export const SITE_DESCRIPTION = "Product designer and founder";
