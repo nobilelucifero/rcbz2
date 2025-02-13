6bd96462a478494e35d0c57616a1bc0e735d1a6d
Riccardo Buzzotta <riccardo.buzzotta@gmail.com>
2025-02-13 10:57:03 +0100

    remove base value from config, hrefs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 // import sitemap from "@astrojs/sitemap";
 
-export const basePath = "rcbz2";
-
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
   site: "https://nobilelucifero.github.io",
-  base: "rcbz2",
   integrations: [mdx()],
   // integrations: [mdx(), sitemap()],
 });
